81e740e833b34bf7cb7566eee87f38ff7ba2df6e
cebiromerfaruk <127345239+cebiromerfaruk@users.noreply.github.com>
2025-08-17 14:55:13 +0300

    feat(android): split features into colorful screens

---
+++ android/app/src/main/java/com/example/lockeduntil/UseCasesActivity.kt
@@ -0,0 +1,11 @@
+package com.lockeduntil.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UseCasesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_use_cases)
+    }
+}
---
+++ android/app/src/main/java/com/example/lockeduntil/WhyTrustActivity.kt
@@ -0,0 +1,11 @@
+package com.lockeduntil.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WhyTrustActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_why_trust)
+    }
+}
